5edd73084e4a893ad3ead01ad60e321672d8a0f9
ROBIN Yoann <yoann.robin.k@gmail.com>
2019-08-20 09:58:00 +0200

    Continuous dynamical system work

--- R/CDSK/R/dynamic.DynamicalSystem.R
+++ R/CDSK/R/dynamic.DynamicalSystem.R
@@ -78,18 +78,17 @@ DynamicalSystem = R6::R6Class( "DynamicalSystem" ,
 	
 	randomIC = function()
 	{
-		return( base::apply( self$bounds , 1 , function(X) { return( stats::runif( n = self$size , min = X[1] , max = X[2] ) ) } ) )
+		return( stats::runif( self$dim * self$size , min = self$bounds[,1] , max = self$bounds[,2] ) )
+#		return( base::apply( self$bounds , 1 , function(X) { return( stats::runif( n = self$size , min = X[1] , max = X[2] ) ) } ) )
 	},
 	
 	orbit = function( t , X0 = NULL )
 	{
-		X0 = if( is.null(X0) ) self$randomIC() else X0
-		X0 = as.vector(base::t(X0))
-		X = private$solver( t , X0 )
+		X0 = if( is.null(X0) ) self$randomIC() else as.vector(base::t(X0))
+		X  = private$solver( t , X0 )
 		if( self$size > 1 )
 		{
-			X  = base::array( X , base::c( dim(X)[1] , self$dim , self$size ) )
-			return( base::aperm( X , base::c( 1 , 3 , 2 ) ) )
+			X  = base::aperm( base::array( X , base::c( dim(X)[1] , self$dim , self$size ) ) , perm = base::c( 1 , 3 , 2 ) )
 		}
 		return( X )
 	}
@@ -118,86 +117,6 @@ DynamicalSystem = R6::R6Class( "DynamicalSystem" ,
 	
 	)
 )
-##}}}
-
-## Local Dimension {{{
-
-#' Local Dimension and persistence
-#'
-#' Compute the local dimension and the persistence of a dataset
-#'
-#' @param X [matrix] A first matrix (time in column, variables in row). Point where you want the local dimension and persistance
-#'
-#' @param Y [matrix] A second matrix (time in column, variables in row). Point to estimate ld and theta. If Y = NULL, X is used
-#'
-#' @param q [float] Threshold, default = 0.98
-#'
-#' @param distXY [matrix] -log of pairwise distances between X and Y. If NULL, computed with CDSK::pairwise_distances( X , Y , metric = "logeuclidean" )
-#'
-#' @param gpd_fit [NULL or function] Function which fit the scale parameter of a gpd distribution, take a vector containing in fist index the threshold, and other values the dataset, and return the scale. If NULL, mean inverse is used.
-#'
-#' @return ld,theta [list] list containing local dimension and theta
-#'
-#' @examples
-#' l63 = CDSK::Lorenz63$new()
-#' t = base::seq( 0 , 100 , 0.005 )
-#' Y = l63$orbit(t)
-#' X = Y[,sample(1:length(t),1000)]
-#' ldt = CDSK::localDimension( X , Y )
-#' print(base::mean(ldt$ld))
-#'
-#' @export
-localDimension = function( X , Y = NULL , q = 0.98 , distXY = NULL , gpd_fit = NULL )
-{
-	## Fit function for theta
-	theta_ferro = function( Z )
-	{
-		iThreshold = which( Z[-1] > Z[1] )
-		l = length(iThreshold)
-		Ti = base::diff(iThreshold)
-		res = 2
-		if( base::max(Ti) > 2 )
-		{
-			res = 2 * ( base::sum(Ti - 1)^2 ) / ( (l-1) * base::sum( (Ti-1) * (Ti-2) ) )
-		}
-		else
-		{
-			res = 2 * base::sum(Ti)^2 / ( (l-1) * base::sum( Ti^2 ) )
-		}
-		res = base::min( 1 , res )
-		return( res )
-	}
-	
-	## Fit function for local dim
-	gpdfit_mean = function(Z)
-	{
-		return( 1. / base::mean( Z[-1][Z[-1] > Z[1]] - Z[1] ) )
-	}
-	
-	if( is.null(gpd_fit) )
-	{
-		gpd_fit = gpdfit_mean
-	}
-	
-	## Pairwise distances
-	if( is.null(distXY) )
-	{
-		distXY = - CDSK::pairwise_distances( X , Y , metric = "logeuclidean" )
-		distXY[which( is.infinite(distXY) )] = - Inf
-	}
-	
-	## Threshold
-	thresholds = base::apply( distXY , 1 , quantile , probs = q )
-	
-	## Fit localdim
-	ld = base::apply( base::cbind( thresholds , distXY ) , 1 , gpd_fit )
-#	ld = base::apply( base::cbind( thresholds , distXY ) , 1 , function( Z ) { return(1. / base::mean( Z[-1][Z[-1] > Z[1]] - Z[1] ) ) } )
-	
-	## Fit theta
-	theta = base::apply( base::cbind( thresholds , distXY ) , 1 , theta_ferro )
-	
-	return( list( ld = ld , theta = theta ) )
-}
 ##}}}
 
 
