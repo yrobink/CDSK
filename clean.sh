4b117e530ed93709f07a211e8c061600a4c560f2
Yoann Robin <yoann.robin.k@gmail.com>
2020-07-20 09:54:56 +0200

    Add a clean file

---
+++ clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## Delete python temporary files
+rm -rf python/CDSK.egg*
+rm -rf python/build
+rm -rf python/dist
+rm -rf python/tmp
+rm -rf python/var
+
+
+## Delete R temporary files
+rm -f R/CDSK/man/*.Rd
+rm -f R/*.tar.gz
+rm -f R/CDSK/src/RcppExports.cpp
+rm -f R/CDSK/R/RcppExports.R
+rm -f R/CDSK/src/*.o
+rm -f R/CDSK/src/*.so
+
