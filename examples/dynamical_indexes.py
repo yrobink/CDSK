e702b5eb4095da425d744718394eec9cc9056806
= <yoann.robin.k@gmail.com>
2021-04-08 12:59:39 +0200

    Add ynamical_local_indexes function

---
+++ examples/dynamical_indexes.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+#############################
+## Yoann Robin             ##
+## yoann.robin.k@gmail.com ##
+#############################
+
+###############
+## Libraries ##
+##{{{
+
+import sys,os
+import itertools as itt
+
+## Scientific libraries
+##=====================
+import numpy as np
+import xarray as xr
+import CDSK as cs
+
+
+## Plot libraries ##
+##==================
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as mplgrid
+
+##}}}
+###############
+
+###############
+## Fonctions ##
+###############
+
+#############
+## Classes ##
+#############
+
+
+##########
+## main ##
+##########
+
+if __name__ == "__main__":
+	
+	np.random.seed(42)
+	
+	f_data = "data_l63.nc"
+	if os.path.exists(f_data):
+		df = xr.open_dataset(f_data)
+	else:
+		## Data
+		l63 = cs.Lorenz63()
+		t0  = np.arange( 0 , 10 , 0.01 )
+		t   = np.arange( 0 , 100 , 0.01 )
+		Y0 = l63.orbit( t0 )[-1,:]
+		Y  = l63.orbit( t , Y0 )
+		
+		n_snap = 1000
+		l63 = cs.Lorenz63( size = n_snap )
+		X = l63.orbit( t )[-1,:,:]
+		
+		## Save
+		R = ["R{:{fill}{align}{n}}".format(i,fill=0,align=">",n=3) for i in range(n_snap)]
+		X     = xr.DataArray( X     , dims = ["ref","axes"]  , coords = [R,["x","y","z"]] )
+		Y     = xr.DataArray( Y     , dims = ["time","axes"] , coords = [t,["x","y","z"]] )
+		df    = xr.Dataset( { "X" : X , "Y" : Y } )
+		df.to_netcdf(f_data)
+	
+	## Arguments
+	kwargs = { "ld_fit" : "SDFC" }
+	
+	## Pairwise projections
+	X2 = np.zeros( (df.ref.size,2,3) )
+	X2[:,:,0] = df.X[:,[0,1]]
+	X2[:,:,1] = df.X[:,[0,2]]
+	X2[:,:,2] = df.X[:,[1,2]]
+	Y2 = np.zeros( (df.time.size,2,3) )
+	Y2[:,:,0] = df.Y[:,[0,1]]
+	Y2[:,:,1] = df.Y[:,[0,2]]
+	Y2[:,:,2] = df.Y[:,[1,2]]
+	ld2,theta2,alpha2,shp2 = cs.dynamical_local_indexes( X2 , Y2 , **kwargs )
+	
+	## 3d attractor
+	X3 = df.X.values
+	Y3 = df.Y.values
+	ld3,theta3,alpha3,shp3 = cs.dynamical_local_indexes( X3 , Y3 , **kwargs )
+	
+	## Figures
+	l_cmap = [plt.cm.inferno,plt.cm.turbo,plt.cm.binary,plt.cm.coolwarm]
+	nrow,ncol = 4,3
+	grid = mplgrid.GridSpec( nrow , ncol , height_ratios = [1,1,1,0.05] )
+	
+	for c,c3d,title,cmap in zip([ld2,theta2,alpha2,shp2],[ld3,theta3,alpha3,shp3],["Local Dimension","Theta","Alpha","Shape"],l_cmap):
+		fig = plt.figure( figsize = (12,12) )
+		
+		vmin2,vmax2 = np.nanquantile( c   , [0.1,0.9] )
+		vmin3,vmax3 = np.nanquantile( c3d , [0.1,0.9] )
+		vmin = min(vmin2,vmin3)
+		vmax = max(vmax2,vmax3)
+		if title == "Shape":
+			vminmax = max(abs(vmin),abs(vmax))
+			vmin = -vminmax
+			vmax = vminmax
+		
+		kwargs = { "vmin" : vmin , "vmax" : vmax , "cmap" : cmap , "s" : 5 }
+		for i,j in itt.combinations_with_replacement(range(3),2):
+			ax = fig.add_subplot( grid[i,j] )
+			
+			if i == j:
+				im = ax.scatter( X2[:,0,i] , X2[:,1,i] , c = c[:,i,j] , **kwargs )
+			else:
+				ax.plot( c[:,i,j] , color = "blue" )
+			
+			ax.set_title( "Mean = {:.2f}".format( np.mean(c[:,i,j]) ) )
+		
+		cax = fig.add_subplot( grid[3,:] )
+		plt.colorbar( mappable = im , cax = cax , orientation = "horizontal" , label = title )
+		
+		ax  = fig.add_subplot( grid[2,0] , projection = "3d" )
+		ax.scatter( X3[:,0] , X3[:,1] , X3[:,2] , c = c3d[:,0,0] , **kwargs )
+		ax.set_title( "Mean = {:.2f}".format( np.mean(c3d) ) )
+		
+		plt.tight_layout()
+		plt.savefig( "fig_{}.png".format(title) )
+		plt.close(fig)
+	
+	
+	
+	print("Done")
+
--- python/CDSK/__dynamical_local_indexes.py
+++ python/CDSK/__dynamical_local_indexes.py
@@ -3,7 +3,7 @@
 ##################################################################################
 ##################################################################################
 ##                                                                              ##
-## Copyright Yoann Robin, 2019                                                  ##
+## Copyright Yoann Robin, 2021                                                  ##
 ##                                                                              ##
 ## yoann.robin.k@gmail.com                                                      ##
 ##                                                                              ##
@@ -44,7 +44,7 @@
 ##################################################################################
 ##################################################################################
 ##                                                                              ##
-## Copyright Yoann Robin, 2019                                                  ##
+## Copyright Yoann Robin, 2021                                                  ##
 ##                                                                              ##
 ## yoann.robin.k@gmail.com                                                      ##
 ##                                                                              ##
@@ -83,33 +83,205 @@
 ##################################################################################
 ##################################################################################
 
-
-
 ###############
 ## Libraries ##
 ###############
 
-__version__ = "0.2.0a0"
-
-## Abstract class
-from .__DynamicalSystem import DynamicalSystem
-from .__DiffDynSyst     import DiffDynSyst
-from .__DiscDynSyst     import DiscDynSyst
-
-
-## Continuous dynamical system
-from .__Lorenz63 import Lorenz63
-from .__Lorenz84 import Lorenz84
-from .__Rossler  import Rossler
+import sys,os
+import itertools as itt
+import numpy as np
+import scipy.stats as sc
+import sklearn.metrics.pairwise as skmp
+import multiprocessing as mp
+import SDFC as sd
 
 
-## Discrete dynamical system
-from .__Henon import Henon
-from .__Ikeda import Ikeda
-from .__Mira  import Mira
+###############
+## Functions ##
+###############
 
+def _theta_sueveges( idx , ql ):##{{{
+	N   = idx.size - 1
+	Nc  = np.sum( idx[1:] - idx[:-1] - 1 > 0 )
+	T   = ( 1. - ql ) * ( idx[-1] - idx[0] )
+	return ( T + N + Nc - np.sqrt( np.power( T + N + Nc , 2. ) - 8. * Nc * T ) ) / ( 2. * T )
+##}}}
 
-## Functions
-from .__LocalDimension import localDimension
-from .__dynamical_local_indexes import dynamical_local_indexes
+def _theta_ferro( idx ): ##{{{
+	Ti = idx[1:] - idx[:-1]
+	if np.max(Ti) > 2:
+		res = 2 * ( np.sum(Ti - 1)**2 ) / ( (Ti.size-1) * np.sum( (Ti-1) * (Ti-2) ) )
+	else:
+		res = 2 * ( np.sum(Ti)**2 ) / ( (Ti.size-1) * np.sum( Ti**2 ) )
+	return min( 1 , res )
+##}}}
+
+def _gpd_fit( data , ld_fit ):##{{{
+	if ld_fit == "scipy":
+		shape,_,scale = sc.genpareto.fit( data , floc = 0 )
+		return np.array( [scale,shape] )
+	else:
+		gpd = sd.GPD()
+		gpd.fit( data , f_loc = 0 )
+		return gpd.coef_
+##}}}
+
+def _local_dimension_parallel( queue , dist , q , ld_fit ): ##{{{
+	size = dist.shape[0]
+	ld   = np.zeros(size)
+	shp  = np.zeros(size)
+	
+	for i in range(size):
+		data = dist[i,dist[i,:] > q[i]] - q[i]
+		ld[i],shp[i] = _gpd_fit( data , ld_fit )
+	
+	queue[0].put(ld)
+	queue[1].put(shp)
+##}}}
+
+def _local_dimension( dist , q , where , ld_fit , n_jobs ):##{{{
+	
+	## Mean estimator
+	if ld_fit not in ["SDFC","scipy"]:
+		return 1. / np.nanmean( np.where( where , dist - q , np.nan ) , 1 ),np.zeros(q.size)
+	
+	## Others
+	n_sample  = q.size
+	l_idx     = np.array_split( range(n_sample) , n_jobs )
+	l_queue   = []
+	l_threads = []
+	for idx in l_idx:
+		l_queue.append( [mp.Queue(),mp.Queue()] )
+		l_threads.append( mp.Process( target = _local_dimension_parallel , args = (l_queue[-1],dist[idx,:],q[idx],ld_fit) ) )
+		l_threads[-1].start()
+	
+	for th in l_threads:
+		th.join()
+	
+
+	ld  = np.zeros(n_sample)
+	shp = np.zeros(n_sample)
+	
+	for idx,queue in zip(l_idx,l_queue):
+		ld[idx]  = 1. / queue[0].get()
+		shp[idx] = queue[1].get()
+	
+	return ld,shp
+##}}}
+
+def dynamical_local_indexes( X , Y = None , ql = 0.98 , ld_fit = "SDFC" , theta_fit = "sueveges" , n_jobs = os.cpu_count() , **kwargs ):##{{{
+	"""
+	CDSK.localDimension
+	===================
+	
+	Description
+	-----------
+	Fit a dataset to find (cross) local dimension, persistence, alpha and shape.
+	
+	Parameters
+	----------
+	X          : np.array[ shape = (n_sample,n_features,n_var) ]
+		Dataset to fit
+	Y          : np.array[ shape = (n_sample_2,n_features,n_var) ] or None
+		Reference to estimate. If Y is None, Y = X
+	ql         : float = 0.98
+		Quantile used to find the threshold for generalized pareto distribution
+	ld_fit     : str = "SDFC"
+		Method to fit the scale of generalized pareto law. Options are : "SDFC"
+		(scale estimation), "scipy" (scale estimation, slower) and "mean"
+		(assume shape = 0).
+	theta_fit  : str = "sueveges"
+		Method to fit the persistence. "ferro" or "sueveges".
+	n_jobs     : int = os.cpu_count()
+		Number of CPU available.
+	metric     : str or callable = "euclidean"
+		Metric used between sample of X and Y, see sklearn.metrics.pairwise.pairwise_distances
+	cross_metric: callable = lambda x,y : np.sqrt(x**2+y**2)
+		Metric used between two variables.
+	
+	
+	Returns
+	-------
+	ld    : np.array[ shape = (n_sample,n_var,n_var) ]
+		Local dimension of elements of X. The vector ld[:,i,j] is the cross-local
+		dimension between X[:,:,i] and X[:,:,j]. Only the upper part of the 
+		matrix is filled.
+	theta : np.array[ shape = (n_sample,n_var,n_var) ]
+		Persistence (also called extremal index) of elements of X. The vector
+		theta[:,i,j] is the cross persistence between X[:,:,i] and X[:,:,j]. 
+		Only the upper part of the matrix is filled.
+	alpha : np.array[ shape = (n_sample,n_var,n_var) ]
+		Co-recurrences of elements of X. The vector alpha[:,i,j] is the
+		co-recurrences between X[:,:,i] and X[:,:,j].  Only the upper part of
+		the matrix is filled. See [1].
+	shape : np.array[ shape = (n_sample,n_var,n_var) ]
+		Shape estimated for the GPD during the fit of local dimension. The
+		vector shape[:,i,j] is the shape between X[:,:,i] and X[:,:,j].  Only
+		the upper part of the matrix is filled. If ld_fit == "mean", shape is
+		always 0.
+	
+	References
+	----------
+	[1] Messori. G. and Faranda, D. (2021) Technical note: Characterising and
+	comparing different palaeoclimates with dynamical systems theory.
+	Clim. Past, 17, 545–563. doi:https://doi.org/10.5194/cp-17-545-2021.
+	"""
+	
+	## Read kwargs
+	metric = kwargs.get("metric")
+	if metric is None: metric = "euclidean"
+	cross_metric = kwargs.get("cross_metric")
+	if cross_metric is None: cross_metric = lambda x,y : np.sqrt(x**2+y**2)
+	
+	## Data in good format
+	if X.ndim == 2:
+		X = X.reshape( X.shape + (1,) )
+	if Y is None:
+		Y = X
+	elif Y.ndim == 2:
+		Y = Y.reshape( Y.shape + (1,) )
+	
+	n_sampleX,n_features,n_traj = X.shape
+	n_sampleY,n_features,n_traj = Y.shape
+	
+	## Distances
+	dist = np.zeros( (n_sampleX,n_sampleY,n_traj,n_traj) )
+	for i in range(n_traj):
+		dist[:,:,i,i] = skmp.pairwise_distances( X[:,:,i] , Y[:,:,i] , metric = metric , n_jobs = n_jobs )
+		dist[:,:,i,i] /= np.linalg.norm( dist[:,:,i,i] , 1 )
+	
+	for i,j in itt.combinations(range(n_traj),2):
+		dist[:,:,i,j] = cross_metric(dist[:,:,i,i],dist[:,:,j,j])
+	
+	dist[~(dist > 0)] = sys.float_info.max
+	dist = -np.log(dist)
+	
+	## Local dimension
+	ld    = np.zeros( (n_sampleX,n_traj,n_traj) ) + np.nan
+	shp   = np.zeros( (n_sampleX,n_traj,n_traj) ) + np.nan
+	q     = np.zeros( (n_sampleX,n_traj,n_traj) ) + np.nan
+	where = np.zeros( (n_sampleX,n_sampleY,n_traj,n_traj) , dtype = bool )
+	for i,j in itt.combinations_with_replacement(range(n_traj),2):
+		q[:,i,j]             = np.quantile( dist[:,:,i,j] , ql , 1 )
+		where[:,:,i,j]       = dist[:,:,i,j] > q[:,i,j].reshape(-1,1)
+		ld[:,i,j],shp[:,i,j] = _local_dimension( dist[:,:,i,j] , q[:,i,j].reshape(-1,1) , where[:,:,i,j] , ld_fit , n_jobs )
+	
+	## Theta
+	theta = np.zeros( (n_sampleX,n_traj,n_traj) ) + np.nan
+	for i,j in itt.combinations_with_replacement(range(n_traj),2):
+		for k in range(n_sampleX):
+			idx = np.argwhere(where[k,:,i,j]).squeeze()
+			if theta_fit == "ferro":
+				theta[k,i,j] = _theta_ferro( idx , ql )
+			else:
+				theta[k,i,j] = _theta_sueveges( idx , ql )
+	
+	## Alpha
+	alpha = np.zeros( (n_sampleX,n_traj,n_traj) ) + np.nan
+	for i,j in itt.combinations_with_replacement(range(n_traj),2):
+		alpha[:,i,j] = np.sum( where[:,:,i,i] & where[:,:,j,j] , 1 ) / np.sum(where[:,:,i,i],1)
+	
+	
+	return ld,theta,alpha,shp
+##}}}
 
