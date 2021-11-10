44735f53bd258d8355decc63a43031dfc9e6604a
Yoann Robin <yoann.robin.k@gmail.com>
2021-11-10 07:45:39 +0100

    Update

--- tests/test.py
+++ tests/test.py
@@ -104,62 +104,62 @@ import scipy.stats as sc
 import scipy.optimize as sco
 
 import matplotlib as mpl
-try:
-	import matplotlib.pyplot as plt
-except:
-	mpl.use("Qt5Agg")
-	import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
-
+import matplotlib.pyplot as plt
 
 import CDSK as ck
 import CDSK.fractal as ckf
 
 
 ####################
-## Paramètres mpl ##
+## mpl parameters ##
 ####################
 
-#mpl.rcParams['font.size'] = 30
-#plt.rc('text',usetex=True)
-#plt.rcParams['text.latex.unicode'] = True
+
+mpl.rcdefaults()
+mpl.rcParams['font.size'] = 7
+mpl.rcParams['axes.linewidth'] = 0.5
+mpl.rcParams['lines.linewidth'] = 0.5
+mpl.rcParams['lines.markersize'] = 1
+mpl.rcParams['patch.linewidth'] = 0.5
+mpl.rcParams['xtick.major.width'] = 0.5
+mpl.rcParams['ytick.major.width'] = 0.5
 
 
 ###############
-## Fonctions ##
+## Functions ##
 ###############
 
-def test_lorenz63( plot = True ):##{{{
+def test_lorenz63():##{{{
 	print( "Test Lorenz63..." , end = "\r" )
 	try:
 		l63 = ck.Lorenz63()
 		X0  = l63.orbit( np.linspace( 0 , 100 , 100 ) )[-1,:]
 		X   = l63.orbit( np.linspace( 0 , 100 , 10000 ) , X0 = X0 )
 		
-		if plot:
-			fig = plt.figure( figsize = (7,7) )
-			ax = fig.add_subplot( 1 , 1 , 1 , projection = "3d" )
-			ax.plot( X[:,0] , X[:,1] , X[:,2] , color = "red" , linestyle = "-" )
-			fig.set_tight_layout(True)
-			plt.show()
+		mm  = 1. / 25.4
+		fig = plt.figure( figsize = (90*mm,90*mm) )
+		ax = fig.add_subplot( 1 , 1 , 1 , projection = "3d" )
+		ax.plot( X[:,0] , X[:,1] , X[:,2] , color = "red" , linestyle = "-" )
+		plt.subplots_adjust( left = 0 , right = 0.95 , bottom = 0 , top = 1 )
+		plt.savefig( "output/test_lorenz63.png" , dpi = 600 )
 		print( "Test Lorenz63 (Done)  " )
 	except:
 		print( "Test Lorenz63 (Fail)  " )
 ##}}}
 
-def test_rossler( plot = True ):##{{{
+def test_rossler():##{{{
 	print( "Test Rossler..." , end = "\r" )
 	try:
 		ross = ck.Rossler()
 		X0  = ross.orbit( np.linspace( 0 , 100 , 100 ) )[-1,:]
 		X   = ross.orbit( np.linspace( 0 , 300 , 10000 ) , X0 = X0 )
 		
-		if plot:
-			fig = plt.figure( figsize = (7,7) )
-			ax = fig.add_subplot( 1 , 1 , 1 , projection = "3d" )
-			ax.plot( X[:,0] , X[:,1] , X[:,2] , color = "red" , linestyle = "-" )
-			fig.set_tight_layout(True)
-			plt.show()
+		mm  = 1. / 25.4
+		fig = plt.figure( figsize = (90*mm,90*mm) )
+		ax = fig.add_subplot( 1 , 1 , 1 , projection = "3d" )
+		ax.plot( X[:,0] , X[:,1] , X[:,2] , color = "red" , linestyle = "-" )
+		plt.subplots_adjust( left = 0 , right = 0.95 , bottom = 0 , top = 1 )
+		plt.savefig( "output/test_rossler.png" , dpi = 600 )
 		print( "Test Rossler (Done)  " )
 	except:
 		print( "Test Rossler (Fail)  " )
@@ -182,14 +182,14 @@ def test_lorenz84( plot = True ):##{{{
 		lX.append( X[t_summ,:,:] )
 		title = ["Fall","Winter","Spring","Summer"]
 		
-		if plot:
-			fig = plt.figure( figsize = (12,12) )
-			for i,X in enumerate(lX):
-				ax = fig.add_subplot( 2 , 2 , i + 1 , projection = "3d" )
-				ax.plot( X[:,0] , X[:,1] , X[:,2] , color = "red" , linestyle = "" , marker = "." )
-				ax.set_title( title[i] , fontsize = 20 )
-			fig.set_tight_layout(True)
-			plt.show()
+		mm  = 1. / 25.4
+		fig = plt.figure( figsize = (90*mm,90*mm) )
+		for i,X in enumerate(lX):
+			ax = fig.add_subplot( 2 , 2 , i + 1 , projection = "3d" )
+			ax.plot( X[:,0] , X[:,1] , X[:,2] , color = "red" , linestyle = "" , marker = "." )
+			ax.set_title( title[i] )
+		plt.subplots_adjust( left = 0 , right = 0.92 , bottom = 0.05 , top = 0.95 )
+		plt.savefig( "output/test_lorenz84.png" , dpi = 600 )
 		print( "Test Lorenz84 (Done)  " )
 	except:
 		print( "Test Lorenz84 (Fail)  " )
@@ -201,12 +201,12 @@ def test_henon( plot = True ):##{{{
 		henon = ck.Henon()
 		X   = henon.orbit( 10000 , X0 = np.array( [0.5,0.5] ) )
 		
-		if plot:
-			fig = plt.figure( figsize = (7,7) )
-			ax = fig.add_subplot( 1 , 1 , 1 )
-			ax.plot( X[:,0] , X[:,1] , color = "red" , linestyle = "" , marker = "." )
-			fig.set_tight_layout(True)
-			plt.show()
+		mm  = 1. / 25.4
+		fig = plt.figure( figsize = (90*mm,90*mm) )
+		ax = fig.add_subplot( 1 , 1 , 1 )
+		ax.plot( X[:,0] , X[:,1] , color = "red" , linestyle = "" , marker = "." )
+		plt.tight_layout()
+		plt.savefig( "output/test_henon.png" , dpi = 600 )
 		print( "Test Henon (Done)  " )
 	except:
 		print( "Test Henon (Fail)  " )
@@ -218,12 +218,12 @@ def test_ikeda( plot = True ):##{{{
 		ike = ck.Ikeda()
 		X   = ike.orbit( 10000 , X0 = np.array( [0.5,0.5] ) )
 		
-		if plot:
-			fig = plt.figure( figsize = (7,7) )
-			ax = fig.add_subplot( 1 , 1 , 1 )
-			ax.plot( X[:,0] , X[:,1] , color = "red" , linestyle = "" , marker = "." )
-			fig.set_tight_layout(True)
-			plt.show()
+		mm  = 1. / 25.4
+		fig = plt.figure( figsize = (90*mm,90*mm) )
+		ax = fig.add_subplot( 1 , 1 , 1 )
+		ax.plot( X[:,0] , X[:,1] , color = "red" , linestyle = "" , marker = "." )
+		plt.tight_layout()
+		plt.savefig( "output/test_ikeda.png" , dpi = 600 )
 		print( "Test Ikeda (Done)  " )
 	except:
 		print( "Test Ikeda (Fail)  " )
@@ -237,12 +237,12 @@ def test_mira( plot = True ):##{{{
 		mir = ck.Mira( size = 10000 )
 		X   = mir.orbit( 1000 , X0 = X0 )[-1,:,:]
 		
-		if plot:
-			fig = plt.figure( figsize = (7,7) )
-			ax = fig.add_subplot( 1 , 1 , 1 )
-			ax.plot( X[:,0] , X[:,1] , color = "red" , linestyle = "" , marker = "." )
-			fig.set_tight_layout(True)
-			plt.show()
+		mm  = 1. / 25.4
+		fig = plt.figure( figsize = (90*mm,90*mm) )
+		ax = fig.add_subplot( 1 , 1 , 1 )
+		ax.plot( X[:,0] , X[:,1] , color = "red" , linestyle = "" , marker = "." )
+		plt.tight_layout()
+		plt.savefig( "output/test_mira.png" , dpi = 600 )
 		print( "Test Mira (Done)  " )
 	except:
 		print( "Test Mira (Fail)  " )
@@ -310,17 +310,19 @@ def test_julia( plot = True ):##{{{
 
 
 def run_all_tests( plot = False ):##{{{
-	test_lorenz63(plot)
-	test_rossler(plot)
-	test_lorenz84(plot)
-	test_henon(plot)
-	test_ikeda(plot)
-	test_mira(plot)
+	if not os.path.isdir("output"):
+		os.makedirs("output")
+#	test_lorenz63()
+#	test_rossler()
+#	test_lorenz84()
+#	test_henon()
+	test_ikeda()
+	test_mira()
 	
-	test_local_dimension(plot)
-	
-	test_mandelbrot(plot)
-	test_julia(plot)
+#	test_local_dimension(plot)
+#	
+#	test_mandelbrot(plot)
+#	test_julia(plot)
 ##}}}
 
 
