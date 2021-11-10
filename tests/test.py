bcccab743b7d6c2c04bb3689546af2bd6bc0352c
Yoann Robin <yoann.robin.k@gmail.com>
2021-11-10 09:28:26 +0100

    Update tests

--- tests/test.py
+++ tests/test.py
@@ -105,6 +105,7 @@ import scipy.optimize as sco
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as mplg
 
 import CDSK as ck
 import CDSK.fractal as ckf
@@ -165,7 +166,7 @@ def test_rossler():##{{{
 		print( "Test Rossler (Fail)  " )
 ##}}}
 
-def test_lorenz84( plot = True ):##{{{
+def test_lorenz84():##{{{
 	print( "Test Lorenz84..." , end = "\r" )
 	try:
 		l63 = ck.Lorenz84( size = 1000 , F = "cyclic" )
@@ -195,7 +196,7 @@ def test_lorenz84( plot = True ):##{{{
 		print( "Test Lorenz84 (Fail)  " )
 ##}}}
 
-def test_henon( plot = True ):##{{{
+def test_henon():##{{{
 	print( "Test Henon..." , end = "\r" )
 	try:
 		henon = ck.Henon()
@@ -212,7 +213,7 @@ def test_henon( plot = True ):##{{{
 		print( "Test Henon (Fail)  " )
 ##}}}
 
-def test_ikeda( plot = True ):##{{{
+def test_ikeda():##{{{
 	print( "Test Ikeda..." , end = "\r" )
 	try:
 		ike = ck.Ikeda()
@@ -229,7 +230,7 @@ def test_ikeda( plot = True ):##{{{
 		print( "Test Ikeda (Fail)  " )
 ##}}}
 
-def test_mira( plot = True ):##{{{
+def test_mira():##{{{
 	print( "Test Mira..." , end = "\r" )
 	try:
 		mir = ck.Mira()
@@ -249,80 +250,146 @@ def test_mira( plot = True ):##{{{
 ##}}}
 
 
-def test_local_dimension( plot = True ):##{{{
+def test_local_dimension():##{{{
 	print( "Test local dimension..." , end = "\r" )
 	try:
+		## Dyn. indexes at points defined by X
+		l63 = ck.Lorenz63( size = 1000 )
+		X01 = l63.orbit( np.linspace( 0 , 100 , 10000 ) )[-1,:,:]
+		
+		## Two reference trajectories
 		l63 = ck.Lorenz63()
-		X0  = l63.orbit( np.linspace( 0 , 100 , 100 ) )[-1,:]
-		X   = l63.orbit( np.linspace( 0 , 100 , 10000 ) , X0 = X0 )
+		y0  = l63.orbit( np.linspace( 0 , 100 , 100 ) )[-1,:]
+		y1  = y0 + np.random.normal( scale = 0.1 , size = 3 )
+		Y0  = l63.orbit( np.linspace( 0 , 100 , 20000 ) , X0 = y0 )
+		Y1  = l63.orbit( np.linspace( 0 , 100 , 20000 ) , X0 = y1 )
 		
-		ld,th = ck.localDimension( X , n_jobs = 6 , pareto_fit = "SDFC" )
-		vmin_ld,vmax_ld = np.quantile( ld , [0.1,0.9] )
-		vmin_th,vmax_th = np.quantile( th , [0.1,0.9] )
+		## Merge
+		X = np.stack( (X01,X01) , -1 )
+		Y = np.stack( (Y0,Y1) , -1 )
+		
+		ld,theta,alpha,shape,where = ck.dynamical_local_indexes( X , Y = Y , ld_fit = "mean" , n_jobs = 6 )
+		
+		fig  = plt.figure()
+		grid = mplg.GridSpec( 6 , 3 )
+		
+		## Local dim
+		ii = [0,1,0]
+		jj = [0,1,1]
+		for i in range(3):
+			ax = fig.add_subplot( grid[i,0] , projection = "3d" )
+			im = ax.scatter( X01[:,0] , X01[:,1] , X01[:,2] , c = ld[:,ii[i],jj[i]] , cmap = plt.cm.inferno , vmin = 1 , vmax = 3 )
+		
+		gax = grid[4,0].subgridspec( 1 , 3 , width_ratios = [0.05,1,0.05] )
+		cax = fig.add_subplot( gax[0,1] )
+		plt.colorbar( mappable = im , cax = cax , orientation = "horizontal" , label = "Local dimension" )
+		
+		## Theta
+		for i in range(3):
+			ax = fig.add_subplot( grid[i,2] , projection = "3d" )
+			im = ax.scatter( X01[:,0] , X01[:,1] , X01[:,2] , c = theta[:,ii[i],jj[i]] , cmap = plt.cm.inferno , vmin = 0 , vmax = 0.5 )
+		
+		gax = grid[4,2].subgridspec( 1 , 3 , width_ratios = [0.05,1,0.05] )
+		cax = fig.add_subplot( gax[0,1] )
+		plt.colorbar( mappable = im , cax = cax , orientation = "horizontal" , label = r"$\theta$" )
 		
 		
-		if plot:
-			fig = plt.figure( figsize = (14,8) )
-			ax = fig.add_axes( [0.05,0.15,0.45,0.8] , projection = "3d" )
-			im_ld = ax.scatter( X[:,0] , X[:,1] , X[:,2] , s = 2. , c = ld , cmap = plt.cm.inferno , vmin = vmin_ld , vmax = vmax_ld )
-			ax.set_title( "Mean local dimension = {}".format( ld.mean().round(2) ) )
-			ax = fig.add_axes( [0.5,0.15,0.45,0.8] , projection = "3d" )
-			im_th = ax.scatter( X[:,0] , X[:,1] , X[:,2] , s = 2. , c = th , cmap = plt.cm.inferno , vmin = vmin_th , vmax = vmax_th )
-			
-			cax = fig.add_axes( [ 0.12 , 0.1 , 0.3 , 0.05 ] )
-			cbar = plt.colorbar( mappable = im_ld , cax = cax , orientation = "horizontal" )
-			cbar.set_label( "Local dimension" )
-			
-			cax = fig.add_axes( [ 0.57 , 0.1 , 0.3 , 0.05 ] )
-			cbar = plt.colorbar( mappable = im_th , cax = cax , orientation = "horizontal" )
-			cbar.set_label( "Persistence" )
-			
-			plt.show()
+		## Figsize
+		mm    = 1. / 25.4
+		width = 110*mm
+		width_cax = width / 2
+		widths = [width_cax,10*mm,width_cax]
+		
+		height_cax = width_cax * ax.get_data_ratio() * 0.77
+		heights = [height_cax for _ in range(3)] + [3*mm] + [5*mm] + [10*mm]
+		height = np.sum(heights)
+		
+		grid.set_height_ratios(heights)
+		grid.set_width_ratios(widths)
+		fig.set_figheight(height)
+		fig.set_figwidth(width)
+		
+		plt.subplots_adjust( left = 0 , bottom = 0 , right = 1 , top = 1 , wspace = 0 , hspace = 0 )
+		plt.savefig( "output/dyn_index.png" , dpi = 600 )
+		plt.close(fig)
 		print( "Test local dimension (Done)  " )
 	except:
 		print( "Test local dimension (Fail)  " )
 ##}}}
 
 
-def test_mandelbrot( plot = True ):##{{{
+def test_mandelbrot():##{{{
 	print( "Test Mandelbrot..." , end = "\r" )
 	try:
 		m = ckf.Mandelbrot( classic_set = "set0" , nx = 1000 , ny = 1000 )
 		m.run()
-		if plot:
-			m.plot(True)
+		
+		##
+		X,Y = np.meshgrid( m.x , m.y )
+		Z   = m.ratio
+		
+		fig = plt.figure()
+		ax  = fig.add_subplot(1,1,1)
+		ax.pcolormesh( X , Y , Z.T , cmap = plt.cm.hot_r , shading = "nearest" )
+		
+		mm    = 1. / 25.4
+		width = 100*mm
+		height = width * ax.get_data_ratio()
+		
+		fig.set_figwidth(width)
+		fig.set_figheight(height)
+		
+		plt.subplots_adjust( left = 0 , bottom = 0 , right = 1 , top = 1 )
+		plt.savefig( "output/test_mandelbrot.png" , dpi = 600 )
+		
 		print( "Test Mandelbrot. (Done)" )
 	except:
 		print( "Test Mandelbrot. (Fail)" )
 ##}}}
 
-def test_julia( plot = True ):##{{{
+def test_julia():##{{{
 	print( "Test Julia..." , end = "\r" )
 	try:
 		jul = ckf.Julia( classic_set = "set0" , nx = 1000 , ny = 1000 )
 		jul.run()
-		if plot:
-			jul.plot(True)
+		
+		##
+		X,Y = np.meshgrid( jul.x , jul.y )
+		Z   = jul.ratio
+		
+		fig = plt.figure()
+		ax  = fig.add_subplot(1,1,1)
+		ax.pcolormesh( X , Y , Z.T , cmap = plt.cm.hot_r , shading = "nearest" )
+		
+		mm    = 1. / 25.4
+		width = 100*mm
+		height = width * ax.get_data_ratio()
+		
+		fig.set_figwidth(width)
+		fig.set_figheight(height)
+		
+		plt.subplots_adjust( left = 0 , bottom = 0 , right = 1 , top = 1 )
+		plt.savefig( "output/test_julia.png" , dpi = 600 )
 		print( "Test Julia. (Done)" )
 	except:
 		print( "Test Julia. (Fail)" )
 ##}}}
 
 
-def run_all_tests( plot = False ):##{{{
+def run_all_tests():##{{{
 	if not os.path.isdir("output"):
 		os.makedirs("output")
-#	test_lorenz63()
-#	test_rossler()
-#	test_lorenz84()
-#	test_henon()
+	test_lorenz63()
+	test_rossler()
+	test_lorenz84()
+	test_henon()
 	test_ikeda()
 	test_mira()
 	
-#	test_local_dimension(plot)
-#	
-#	test_mandelbrot(plot)
-#	test_julia(plot)
+	test_local_dimension()
+	
+	test_mandelbrot()
+	test_julia()
 ##}}}
 
 
@@ -342,7 +409,8 @@ if __name__ == "__main__":
 	print(ck.__version__)
 	
 	## Now tests
-	run_all_tests( ("--plot" in sys.argv) )
+	run_all_tests()
+	
 	
 	print("Done")
 
