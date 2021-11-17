34d051237ac91ee8235bd3c2a95adc4016e2b8a0
Yoann Robin <yoann.robin.k@gmail.com>
2021-11-17 12:57:57 +0100

    version 1.0.0

--- python/CDSK/__release.py
+++ python/CDSK/__release.py
@@ -20,7 +20,7 @@
 version_major = 1
 version_minor = 0
 version_patch = 0
-version_extra = "a2"
+version_extra = ""
 version       = "{}.{}.{}{}".format(version_major,version_minor,version_patch,version_extra)
 
 
